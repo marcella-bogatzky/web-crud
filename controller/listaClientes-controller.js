6d9594d739dfed2a749d386d5f6a4d511c297830
Marcella Bogatzky <marcella.bogatzky@hotmail.com>
2024-05-08 23:21:26 -0300

    Finalização do curso

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -1,31 +1,70 @@
 import { clienteService } from '../service/cliente-service.js'; //IMPORTANDO DE "cliente-service.js"
 
 // GERANDO TEMPLATE
-const criaNovaLinha = (nome, email) => { //Como o código abaixo está em linguagem HTML, é necessário usar o `` para que o JS entenda como uma string. Os código ${} estão em JS
+const criaNovaLinha = (nome, email, id) => { //Como o código abaixo está em linguagem HTML, é necessário usar o `` para que o JS entenda como uma string. Os código ${} estão em JS
     const linhaNovoCliente = document.createElement('tr')
     const conteudo = ` 
         <td class="td" data-td>${nome}</td>
         <td>${email}</td>
         <td>
             <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
                 <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
             </ul>
         </td>
     `
     linhaNovoCliente.innerHTML = conteudo
+    linhaNovoCliente.dataset.id = id
+
     return linhaNovoCliente
 }
 
 // PERCORRENDO ÁRVORE DO DOM
 const tabela = document.querySelector('[data-tabela]')
 
-// FAZ JUNÇÃO COM LISTA CLIENTES
-clienteService.listaClientes()
-.then (data => {
-    data.forEach(elemento => {
-        tabela.appendChild(
-            criaNovaLinha(elemento.nome, elemento.email)
-        )
-    })
+tabela.addEventListener('click', async (evento) => {
+    let ehBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir'
+    if(ehBotaoDeletar){
+        try {
+            const linhaCliente = evento.target.closest('[data-id]')
+            let id = linhaCliente.dataset.id
+            await clienteService.removeCliente(id)
+            linhaCliente.remove()
+        }
+        catch(erro){
+            console.log(erro)
+            window.location.href = '../telas/erro.html'
+        }
+    }
 })
+
+// FAZ JUNÇÃO COM LISTA CLIENTES
+
+const render = async () => {
+    try {
+        const listaClientes = await clienteService.listaClientes()
+    
+        listaClientes.forEach(elemento => {
+            tabela.appendChild(
+                criaNovaLinha(elemento.nome, elemento.email, elemento.id)
+            )
+        })
+    }
+    catch(erro) {
+        console.log(erro)
+        window.location.href = '../telas/erro.html'
+    }
+   
+}
+
+render()
+
+// Abaixo está o primeiro código que fizemos para a junção com lista de cliente. Para sintetiza-lo, acrecentamos o "async" para indicar que se trata de uma função assíncrona e "await" para indicar onde retomar após esperar o retorno no promise. Dessa forma, pudemos retirar o then
+// clienteService.listaClientes()
+// .then (data => {
+//     data.forEach(elemento => {
+//         tabela.appendChild(
+//             criaNovaLinha(elemento.nome, elemento.email, elemento.id)
+//         )
+//     })
+// })
--- service/cliente-service.js
+++ service/cliente-service.js
@@ -22,12 +22,79 @@
 const listaClientes = () => { //Esse código tem a mesma função do código comentado acima. É uma forma mais organizada e enxuta de escrevê-lo
     return fetch(`http://localhost:3000/profile`) //Método global da interface da fetch API > por padrão, já faz um GET e retorna uma promise
     .then (resposta => {
-        return resposta.json()
+        if (resposta.ok) { // Esse IF está aqui para fazer um tratamento de erro (retornar o erro para quem esta utilizando a página)
+            return resposta.json()
+        }
+        throw new Error ('Não foi possível listar os clientes')
     })
 }
 
-//EXPORTANDO PARA "listaClientes-controller.js"
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then (resposta => {
+        if(resposta.ok) {
+            return resposta.body
+        }
+        throw new Error ('Não foi possível criar o cliente')
+    })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+    .then (resposta => {
+        if(!resposta.ok) {
+            throw new Error ('Não foi possível remover o cliente')   
+        }
+    })
+}
+
+const detalhaCliente = (id) => {
+return fetch(`http://localhost:3000/profile/${id}`)
+.then (resposta => {
+    if (resposta.ok) {
+        return resposta.json()
+    }
+    throw new Error ('Não foi possível detalhar o cliente')
+})
+
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify ({
+        nome: nome,
+        email: email
+        })
+    })
+    .then (resposta => {
+        if (resposta.ok) { 
+            return resposta.json()
+        }
+        throw new Error ('Não foi possível atualizar o cliente')
+    })
+}
+
+//EXPORTANDO PARA OUTRAS PASTAS
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
 
